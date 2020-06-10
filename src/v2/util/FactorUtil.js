f199bfd8f6f9d9047448ddc2235008f9042e7605
Ani Pendakur <Ani.pendakur@okta.com>
2020-06-09 19:44:00 -0700

    feat: phone authenticator enroll
    Support for phone authenticator enroll through voice or sms.
    Added necessary CSS.
    Added tests and also support for InputGroup type inbox from courage for the sign in widget.
    
    Resolves OKTA-300141

--- src/v2/util/FactorUtil.js
+++ src/v2/util/FactorUtil.js
@@ -27,8 +27,7 @@ const factorData = {
   'phone': {
     label: loc('factor.phone', 'login'),
     description: '',
-    // TODO: need new icon for `mfa-okta-phone`
-    iconClassName: 'mfa-okta-call',
+    iconClassName: 'mfa-okta-phone',
   },
 
   'webauthn': {
